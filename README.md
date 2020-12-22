9f91f009fcfe2d5d4389660dcccae551cdd9827e
Kraivit Mongkhonsakunrit <kraivit.m@edvisory.co.th>
2020-12-22 13:10:07 +0700

    22

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# webin_sqlite
+# webin_sqlite [![Build Status](https://github.com/ez-kraivit/webin_sqlite)](https://github.com/ez-kraivit/webin_sqlite) [![Build status](https://github.com/ez-kraivit/webin_sqlite)](https://github.com/ez-kraivit/webin_sqlite)
+
+## Installation
+
+```bash
+npm install webin_sqlite
+```
+## Usage
+
+```js
+const db = require('webin_sqlite')
+
+const members = db.show_columnname('hello.db','member')
+console.log(members);
+```
+
+# License
+
+[MIT](./LICENSE)
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-module.exports = {
-    checkname : function(options){
-        return 'Hello'
+/*!
+ * etag
+ * Copyright(c) 2020 Kraivit Mongkhonsakunrit
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Module exports.
+ * @public
+ */
+const db = require('better-sqlite3')
+class webin_sqlite { 
+    constructor() {
+        this.report = [];
+      }
+    static show_columnname = (path,name)=>{
+        const tablename = db(path).pragma("table_info('"+name+"')");
+        tablename.map((x)=>{
+            this.report.push(x.name) 
+        });
+        return this.report
     }
 }
+module.exports = webin_sqlite;
